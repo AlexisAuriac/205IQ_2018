842de9f0b39814010367b4d216b37cacd01ce255
alexis <alexis.auriac@epitech.eu>
2019-04-18 03:53:24 +0200

    Isolated each study in seperate functions

--- src/main.go
+++ src/main.go
@@ -5,23 +5,35 @@ import (
 	"os"
 )
 
+func studyDensity(u float64, s float64) {
+	for i := 0.0; i <= 200; i++ {
+		fmt.Printf("%d %.5f\n", int(i), NormDistrib(u, s, i))
+	}
+}
+
+func studyIQBelow(u float64, s float64, iq float64) {
+	fmt.Printf("%.1f%% of people have an IQ inferior to %d\n",
+		CumulNormDistrib(u, s, iq)*100-
+			CumulNormDistrib(u, s, 0)*100,
+		int(iq))
+}
+
+func studyIQBetween(u float64, s float64, iq1 float64, iq2 float64) {
+	fmt.Printf("%.1f%% of people have an IQ between %d and %d\n",
+		CumulNormDistrib(u, s, iq2)*100-
+			CumulNormDistrib(u, s, iq1)*100,
+		int(iq1),
+		int(iq2))
+}
+
 func main() {
 	var params *Params = ParseArgv(os.Args[1:])
 
 	if params.study == Density {
-		for i := 0.0; i <= 200; i++ {
-			fmt.Printf("%d %.5f\n", int(i), NormDistrib(params.u, params.s, i))
-		}
+		studyDensity(params.u, params.s)
 	} else if params.study == PercentageBelow {
-		fmt.Printf("%.1f%% of people have an IQ inferior to %d\n",
-			CumulNormDistrib(params.u, params.s, params.iq1)*100-
-				CumulNormDistrib(params.u, params.s, 0)*100,
-			int(params.iq1))
+		studyIQBelow(params.u, params.s, params.iq1)
 	} else if params.study == PercentageBetween {
-		fmt.Printf("%.1f%% of people have an IQ between %d and %d\n",
-			CumulNormDistrib(params.u, params.s, params.iq2)*100-
-				CumulNormDistrib(params.u, params.s, params.iq1)*100,
-			int(params.iq1),
-			int(params.iq2))
+		studyIQBetween(params.u, params.s, params.iq1, params.iq2)
 	}
 }
